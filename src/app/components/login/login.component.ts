987ad6a0f22105fbab5a438c519eef722216d176
Juan Jimenez <juanpij23@hotmail.com>
2023-02-01 13:03:08 -0600

    Clase01

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  email: String;
+  password: String;
+
+  constructor() { 
+    this.email = "";
+    this.password = "";
+  }
 
   ngOnInit(): void {
+    // TODO document why this method 'ngOnInit' is empty
+  
+  }
+
+  login(){
+    console.log("Email: " + this.email);
+    console.log("Password: " + this.password);
+
+    if(this.email == "admin" && this.password == "linux"){
+      console.log("Bienvenido Administrador");
+    }else{
+      console.log("Acceso Denegado");
+    }
   }
 
 }
