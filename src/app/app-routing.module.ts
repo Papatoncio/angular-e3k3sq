217f06b8f1d2284822a70cc1e81f4d320cfe433e
Juan Jimenez <juanpij23@hotmail.com>
2023-02-04 14:15:15 -0600

    Comi

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
   {
-    path: '', component: AppComponent, pathMatch:"full"
+    path: '', redirectTo: '/home', pathMatch: 'full'
   },
   {
-    path: 'login', component: LoginComponent, pathMatch:"full"
+    path: 'home', component: HomeComponent, pathMatch: "full"
   },
   {
-    path: 'register', component:RegisterComponent, pathMatch:"full"
+    path: 'login', component: LoginComponent, pathMatch: "full"
+  },
+  {
+    path: 'register', component: RegisterComponent, pathMatch: "full"
   }
 ];
 
